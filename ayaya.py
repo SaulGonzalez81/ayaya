072bbdc61a32177f656c4f089b34d4fa6fe0dfee
Aaron Chen <achen178@ucr.edu>
2018-11-11 14:58:43 -0800

    First test run

---
+++ ayaya.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+
+@client.event
+async def on_message(message):
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:")
+
+client.run("NTExMjYwODQwNjgzNTAzNjE2.DspB7Q.ygmroqzlXwW-0R0WjyKmbE6gEak")
