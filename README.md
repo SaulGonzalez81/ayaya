c2dcf810480c71a928ebfebd820f9c1117e8913a
Aaron Chen <achen178@ucr.edu>
2018-11-11 21:02:20 -0800

    why

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Cutie Hack Discord Bot
 
 This bot has a single purpose: to give cookies to any one who asks for one. All you have to do to send any message containing "cookie".
-Ayaya will say, "Here, (username), have a cookie!" and will send a cookie emoji after it, followed by an image of a cookie.
+Ayaya will say, "Here, (username), have a cookie!" and will send a cookie emoji after it.
 
 It serves no other purpose.
 
--- ayaya.py
+++ ayaya.py
@@ -16,13 +16,6 @@ async def on_ready():
 async def on_message(message):
     if ("cookie" in message.content) and not (message.author.top_role.name == "@everyone"):
         await client.send_message(message.channel, "Here, " + message.author.mention + ", have a cookie! :cookie:")
-        imgList = os.listdir("./Cookies") # Creates a list of filenames from your folder
-
-        imgString = random.choice(imgList) # Selects a random element from the list
-
-        path = "./Cookies/" + imgString # Creates a string for the path to the file
-
-        await client.send_file(message.channel, path)
 
 
 client.run("NTExMjYwODQwNjgzNTAzNjE2.DspB7Q.ygmroqzlXwW-0R0WjyKmbE6gEak")
